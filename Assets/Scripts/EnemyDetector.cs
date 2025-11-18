3a3a816aec4d00a250dbab15321662d011e6a177
ahnseojin1121 <way898731@gmail.com>
2025-11-18 14:39:56 +0900

    [Feat] Chain Shoot 완료

---
+++ Assets/Scripts/EnemyDetector.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    [SerializeField] private float detectionRadius = 50f;
+    [SerializeField] private LayerMask enemyLayer;
+
+    public GameObject GetClosestEnemy()
+    {
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, detectionRadius, enemyLayer);
+
+        if (enemiesInRange.Length > 0)
+        {
+            GameObject bestTarget = null;
+            float closestDistanceSqr = Mathf.Infinity;
+            Vector3 currentPosition = transform.position;
+
+            foreach (Collider enemyCollider in enemiesInRange)
+            {
+                // 자기 자신은 제외
+                if (enemyCollider.gameObject == this.gameObject)
+                    continue;
+
+                Vector3 directionToTarget = enemyCollider.transform.position - currentPosition;
+                float dSqrToTarget = directionToTarget.sqrMagnitude;
+
+                if (dSqrToTarget < closestDistanceSqr)
+                {
+                    closestDistanceSqr = dSqrToTarget;
+                    bestTarget = enemyCollider.gameObject;
+                }
+            }
+            return bestTarget;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public List<GameObject> GetEnemiesInRange()
+    {
+        List<GameObject> enemiesList = new List<GameObject>();
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, detectionRadius, enemyLayer);
+
+        foreach (Collider enemyCollider in enemiesInRange)
+        {
+            // 자기 자신은 제외
+            if (enemyCollider.gameObject != this.gameObject)
+            {
+                enemiesList.Add(enemyCollider.gameObject);
+            }
+        }
+
+        return enemiesList;
+    }
+
+    // 에디터에서 범위 확인용
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
